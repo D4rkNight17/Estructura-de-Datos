2d67e4aca3feaa30a74a68a5ef381e9d866ba62c
D4rkNight17 <mlarapuc.10321b@gmail.com>
2025-09-21 21:11:06 -0600

    Arreglos1.1

--- EJER _ARREGLOS/EJER_ARREGLOS.java
+++ EJER _ARREGLOS/EJER_ARREGLOS.java
@@ -1,4 +1,4 @@
-public class EJER_ARREGLOS01{
+public class EJER_ARREGLOS{
     static String[] meses = {"Enero","Febrero","Marzo","Abril","Mayo","Junio",
                              "Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
     static String[] departamentos = {"Ropa","Deportes","Juguetería"};
